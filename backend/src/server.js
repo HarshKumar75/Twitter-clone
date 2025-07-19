a2e3f72fa9e6520cdbde28610aa4513451b39055
Harsh Kumar Panjiyara <harshpanjiyara75@gmail.com>
2025-07-19 20:36:36 +0530

    user routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,29 @@
 import express from 'express';
+import cors from 'cors';
+
+import userRoutes from './routes/user.route.js';
+
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
+import {clerkMiddleware} from '@clerk/express';
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(clerkMiddleware());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');   
 });
 
+app.use("/api/user", userRoutes);
+
 const startServer = async () => {
   try {
     await connectDB();
 
-    // listen for local development
     if (ENV.NODE_ENV !== "production") {
       app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
     }
