a78d793f8ecb97860a78ecd45e849b88993249aa
Harsh Kumar Panjiyara <harshpanjiyara75@gmail.com>
2025-07-23 13:51:18 +0530

    connecting database

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,15 +1,23 @@
 import { Stack } from "expo-router";
 import { ClerkProvider } from "@clerk/clerk-expo";
-import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import "../global.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import * as WebBrowser from "expo-web-browser"; // ✅ ADD THIS LINE
 
+WebBrowser.maybeCompleteAuthSession(); // ✅ MUST BE CALLED OUTSIDE COMPONENT
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      </Stack>
+      <QueryClientProvider client={queryClient}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(auth)" />
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </QueryClientProvider>
     </ClerkProvider>
   );
 }
